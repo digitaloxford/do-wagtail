05c53101c2066fc56b710591e162414dfbe74398
Garrett Coakley <garrettc@gmail.com>
2020-01-04 13:20:02 +0000

    Page layout and image support
    Signed-off-by: Garrett Coakley <garrettc@gmail.com>

--- home/models.py
+++ home/models.py
@@ -3,13 +3,29 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.models import Image
+from wagtail.images.edit_handlers import ImageChooserPanel
 
+class BasicPage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
 
 class HomePage(Page):
+    banner_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
     intro = RichTextField(blank=True)
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
+        ImageChooserPanel('banner_image'),
         FieldPanel('intro', classname="intro"),
         FieldPanel('body', classname="full"),
     ]
