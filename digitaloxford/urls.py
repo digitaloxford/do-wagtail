958a0516da86cfe94fe0d9e5537fd343ad0e9eac
Garrett Coakley <garrettc@gmail.com>
2020-01-05 14:08:00 +0000

    Add templateview for serviceworker and browserconfig
    Signed-off-by: Garrett Coakley <garrettc@gmail.com>

--- digitaloxford/urls.py
+++ digitaloxford/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -15,6 +16,14 @@ urlpatterns = [
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
+
+    # Service worker
+    url(r'^serviceworker.js', (TemplateView.as_view(template_name="serviceworker.js",
+                                                    content_type='application/javascript',)), name='serviceworker.js'),
+
+    # Microsoft Tile config
+    url(r'^browserconfig.xml', (TemplateView.as_view(template_name="browserconfig.xml",
+                                                     content_type='application/xml', )), name='browserconfig.xml'),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
