ae1117fe4da7bf0588d927f29da24f5c868a7671
Garrett Coakley <garrettc@gmail.com>
2020-01-10 22:31:13 +0000

    I overthought things.
    Signed-off-by: Garrett Coakley <garrettc@gmail.com>

--- digitaloxford/wsgi.py
+++ digitaloxford/wsgi.py
@@ -11,9 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-if os.path.exists(os.getcwd() + '/env.py'):
-    # env.py is excluded by .gitignore
-    from .env import *
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "digitaloxford.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "digitaloxford.settings.production")
 
 application = get_wsgi_application()
