e06edd0bd6358391cd6544b78e9a66183550e9ef
Garrett Coakley <garrettc@gmail.com>
2020-01-10 22:42:42 +0000

    Updated settings for deployment

--- digitaloxford/settings/production.py
+++ digitaloxford/settings/production.py
@@ -5,17 +5,30 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
 
 ALLOWED_HOSTS = ['.digitaloxford.com']
 
-TEMPLATES += [
+TEMPLATES = [
     {
+	'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(PROJECT_DIR, 'templates'),
+        ],
         'OPTIONS': {
             'loaders': [
                 ('django.template.loaders.cached.Loader', [
                     'django.template.loaders.filesystem.Loader',
                     'django.template.loaders.app_directories.Loader',
                 ]),
+            ],
+   	    'context_processors': [
+               	'django.template.context_processors.debug',
+               	'django.template.context_processors.request',
+               	'django.contrib.auth.context_processors.auth',
+               	'django.contrib.messages.context_processors.messages',
+
+                'wagtailmenus.context_processors.wagtailmenus',
             ],
         },
     }
