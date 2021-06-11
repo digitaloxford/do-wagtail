ccac7d9583172a99d0eeaef881b2acae4b76348d
Garrett Coakley <garrettc@gmail.com>
2021-06-11 14:19:41 +0100

    Add django extensions and testing framework

--- digitaloxford/settings/base.py
+++ digitaloxford/settings/base.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
+    "coverage",
 ]
 
 MIDDLEWARE = [
