e92533ab0b808316d8973a64b597457522d513f3
Garrett Coakley <garrettc@gmail.com>
2022-02-10 16:26:38 +0000

    Write tests that pass.

--- jobs/tests.py
+++ jobs/tests.py
@@ -2,7 +2,7 @@ from wagtail.tests.utils import WagtailPageTests
 
 from home.models import BasicPage, HomePage
 
-from .models import JobIndexPage, JobPage
+from .models import JobIndexPage, JobPage, RecruiterPage
 
 
 class JobIndexPageTests(WagtailPageTests):
@@ -15,15 +15,33 @@ class JobIndexPageTests(WagtailPageTests):
         self.assertCanNotCreateAt(BasicPage, JobIndexPage)
 
 
-class JobPageTests(WagtailPageTests):
+class RecruiterPageTests(WagtailPageTests):
     def test_can_create_under_job_index_page(self):
-        # You can create a JobPage under a JobIndexPage
-        self.assertCanCreateAt(JobIndexPage, JobPage)
+        # You can create a RecruiterPage under a JobIndexPage
+        self.assertCanCreateAt(JobIndexPage, RecruiterPage)
+
+    def test_cant_create_under_home_page(self):
+        # You can not create a RecruiterPage under the HomePage
+        self.assertCanNotCreateAt(HomePage, RecruiterPage)
+
+    def test_job_page_parent_pages(self):
+        # A RecruiterPage can only be created under a JobIndexPage
+        self.assertAllowedParentPageTypes(RecruiterPage, {JobIndexPage})
+
+
+class JobPageTests(WagtailPageTests):
+    def test_can_create_under_recruiter_page(self):
+        # You can create a JobPage under a RecruiterPage
+        self.assertCanCreateAt(RecruiterPage, JobPage)
+
+    def test_job_page_parent_pages(self):
+        # A JobPage can only be created under a RecruiterPage
+        self.assertAllowedParentPageTypes(JobPage, {RecruiterPage})
 
     def test_cant_create_under_home_page(self):
         # You can not create a JobPage under the HomePage
         self.assertCanNotCreateAt(HomePage, JobPage)
 
-    def test_job_page_parent_pages(self):
-        # A JobPage can only be created under a JobIndexPage
-        self.assertAllowedParentPageTypes(JobPage, {JobIndexPage})
+    def test_cant_create_under_job_index_page(self):
+        # You can not create a JobPage under the JobIndexPage
+        self.assertCanNotCreateAt(JobIndexPage, JobPage)
