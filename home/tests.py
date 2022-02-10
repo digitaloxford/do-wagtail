6a3641df6dbacc8c0513ac03307c4cd294ab76a3
Garrett Coakley <garrettc@gmail.com>
2022-02-10 16:11:01 +0000

    Add tests for home and jobs apps

---
+++ home/tests.py
@@ -0,0 +1,32 @@
+from wagtail.tests.utils import WagtailPageTests
+
+from jobs.models import JobIndexPage, JobPage
+from links.models import LinkIndexPage, LinkPage
+
+from .models import BasicPage, HomePage
+
+
+class HomePageTests(WagtailPageTests):
+    def test_home_page_subpages(self):
+        # A HomePage can only have other BasicPage and *IndexPage children
+        self.assertAllowedSubpageTypes(
+            HomePage, {BasicPage, LinkIndexPage, JobIndexPage}
+        )
+
+    def test_cant_create_under_basic_page(self):
+        # You can not create a HomePage under a BasicPage
+        self.assertCanNotCreateAt(BasicPage, HomePage)
+
+
+class BasicPageTests(WagtailPageTests):
+    def test_can_create_under_home_page(self):
+        # You can create a BasicPage under a HomePage
+        self.assertCanCreateAt(HomePage, BasicPage)
+
+    def test_cant_create_under_link_index_page(self):
+        # You can not create a BasicPage under the LinkIndexPage
+        self.assertCanNotCreateAt(BasicPage, LinkIndexPage)
+
+    def test_cant_create_under_job_index_page(self):
+        # You can not create a BasicPage under the JobIndexPage
+        self.assertCanNotCreateAt(BasicPage, JobIndexPage)
--- jobs/tests.py
+++ jobs/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from wagtail.tests.utils import WagtailPageTests
 
-# Create your tests here.
+from home.models import BasicPage, HomePage
+
+from .models import JobIndexPage, JobPage
+
+
+class JobIndexPageTests(WagtailPageTests):
+    def test_can_create_under_home_page(self):
+        # You can create a JobIndexPage under the HomePage
+        self.assertCanCreateAt(HomePage, JobIndexPage)
+
+    def test_cant_create_under_basic_page(self):
+        # You can not create a JobIndexPage under a BasicPage
+        self.assertCanNotCreateAt(BasicPage, JobIndexPage)
+
+
+class JobPageTests(WagtailPageTests):
+    def test_can_create_under_job_index_page(self):
+        # You can create a JobPage under a JobIndexPage
+        self.assertCanCreateAt(JobIndexPage, JobPage)
+
+    def test_cant_create_under_home_page(self):
+        # You can not create a JobPage under the HomePage
+        self.assertCanNotCreateAt(HomePage, JobPage)
+
+    def test_job_page_parent_pages(self):
+        # A JobPage can only be created under a JobIndexPage
+        self.assertAllowedParentPageTypes(JobPage, {JobIndexPage})
